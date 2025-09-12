b904f9316827f176485115a342e4bb151f517e74
Theophilus Confidence <spanzytheo@gmail.com>
2025-09-12 22:09:57 +0100

    create privacy page

--- app/(users)/terms-and-conditions/page.tsx
+++ app/(users)/terms-and-conditions/page.tsx
@@ -5,7 +5,7 @@ import ForRiders from "../components/terms-and-condition/ForRiders";
 const TermsAndConditions = () => {
   return (
     <>
-      <div className="flex py-3 md:py-4 px-6 md:px-8 flex-col items-center justify-center lg:mt-[6rem]">
+      <div className="flex py-3 md:py-4 px-6 md:px-8 flex-col items-center justify-center mt-[6rem]">
         <div className="flex flex-col w-full items-center gap-2 md:gap-4 my-[3rem] md:my-[4rem]">
           <h1 className="text-center unageo text-[2rem] md:text-[3.75rem] font-bold leading-[2.5rem] md:leading-[4.25rem]">
             Terms and Conditions
