ac822ffd57ef7a574fb48619e6f618eee97160f9
Theophilus Confidence <spanzytheo@gmail.com>
2025-09-13 09:39:50 +0100

    add links to footer

--- app/(users)/components/footer/Socials.tsx
+++ app/(users)/components/footer/Socials.tsx
@@ -1,9 +1,14 @@
-import Image from 'next/image';
+import Image from "next/image";
+import Link from "next/link";
 
 const Socials = () => {
   return (
     <div className="flex items-center md:gap-3 gap-2">
-      <div className="bg-white flex items-center justify-center w-7 md:w-9 h-7 md:h-9 p-2 md:p-3 rounded-full">
+      <Link
+        href={"https://www.instagram.com/dispatchhub01/"}
+        target="_blank"
+        className="bg-white flex items-center justify-center w-7 md:w-9 h-7 md:h-9 p-2 md:p-3 rounded-full"
+      >
         <Image
           width={36}
           height={36}
@@ -11,8 +16,12 @@ const Socials = () => {
           alt="instagram"
           className="w-full h-full object-cover"
         />
-      </div>
-      <div className="bg-white flex items-center justify-center w-7 md:w-9 h-7 md:h-9 p-2 md:p-3 rounded-full">
+      </Link>
+      <Link
+        href={" https://www.facebook.com/dispatchhub01"}
+        target="_blank"
+        className="bg-white flex items-center justify-center w-7 md:w-9 h-7 md:h-9 p-2 md:p-3 rounded-full"
+      >
         <Image
           width={36}
           height={36}
@@ -20,8 +29,12 @@ const Socials = () => {
           alt="facebook"
           className="w-full h-full object-cover"
         />
-      </div>
-      <div className="bg-white flex items-center justify-center w-7 md:w-9 h-7 md:h-9 p-2 md:p-3 rounded-full">
+      </Link>
+      <Link
+        href={"https://x.com/Dispatch_Hub01"}
+        target="_blank"
+        className="bg-white flex items-center justify-center w-7 md:w-9 h-7 md:h-9 p-2 md:p-3 rounded-full"
+      >
         <Image
           width={36}
           height={36}
@@ -29,8 +42,12 @@ const Socials = () => {
           alt="twitter"
           className="w-full h-full object-cover"
         />
-      </div>
-      <div className="bg-white flex items-center justify-center w-7 md:w-9 h-7 md:h-9 p-2 md:p-3 rounded-full">
+      </Link>
+      <Link
+        href={"https://www.linkedin.com/company/dispatchhub/?viewAsMember=true"}
+        target="_blank"
+        className="bg-white flex items-center justify-center w-7 md:w-9 h-7 md:h-9 p-2 md:p-3 rounded-full"
+      >
         <Image
           width={36}
           height={36}
@@ -38,9 +55,9 @@ const Socials = () => {
           alt="linkedin"
           className="w-full h-full object-cover"
         />
-      </div>
+      </Link>
     </div>
   );
-}
+};
 
-export default Socials
+export default Socials;
